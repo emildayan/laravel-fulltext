b88ae8a4817a1c20c144eea8955d86179cd9a238
emildayan <emil@emildayan.se>
2021-10-27 11:33:26 +0200

    Update Indexer.php

--- src/Indexer.php
+++ src/Indexer.php
@@ -6,15 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Indexer
 {
-    public function indexModel(Model $model)
-    {
-        $model->indexRecord();
-    }
-
     public function unIndexOneByClass($class, $id)
     {
         $record = IndexedRecord::where('indexable_id', $id)->where('indexable_type', $class);
-        if ($record->exists) {
+        if ($record->exists)
+        {
             $record->delete();
         }
     }
@@ -22,35 +18,41 @@ class Indexer
     public function indexOneByClass($class, $id)
     {
         $model = call_user_func([$class, 'find'], $id);
-        if (in_array(Indexable::class, class_uses($model), true)) {
+        if (in_array(Indexable::class, class_uses($model), true))
+        {
             $this->indexModel($model);
         }
     }
 
+    public function indexModel(Model $model)
+    {
+        $model->indexRecord();
+    }
+
     public function indexAllByClass($class)
     {
-        $model = new $class();
-        $self = $this;
-        if (in_array(Indexable::class, class_uses($model), true)) {
-            
-            if(in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($model))
-               {
-                   $model->withTrashed()->chunk(100, function ($chunk) use ($self) {
-                foreach ($chunk as $modelRecord) {
-                    $self->indexModel($modelRecord);
-                }
-            });
-               }
-               else
-               {
-                   $model->chunk(100, function ($chunk) use ($self) {
-                foreach ($chunk as $modelRecord) {
-                    $self->indexModel($modelRecord);
-                }
-            });
-               }
-            
-            
+        $model = new $class;
+        $self  = $this;
+        if (in_array(Indexable::class, class_uses($model), true))
+        {
+            if (in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($model)))
+            {
+                $model->withTrashed()->chunk(100, function($chunk) use ($self) {
+                    foreach ($chunk as $modelRecord)
+                    {
+                        $self->indexModel($modelRecord);
+                    }
+                });
+            }
+            else
+            {
+                $model->chunk(100, function($chunk) use ($self) {
+                    foreach ($chunk as $modelRecord)
+                    {
+                        $self->indexModel($modelRecord);
+                    }
+                });
+            }
         }
     }
 }
